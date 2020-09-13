706066d9df1a77350a187286bccf50225f77f628
Marian Hahne <marian.hahne@outlook.com>
2020-09-13 16:17:55 +0200

    IN-27 Create controller for documents

---
+++ src/main/java/de/drentech/innihald/documentrepository/document/DocumentController.java
@@ -0,0 +1,24 @@
+package de.drentech.innihald.documentrepository.document;
+
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.api.CheckedTemplate;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/ui/document")
+@Produces(MediaType.TEXT_HTML)
+public class DocumentController {
+
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance list();
+    }
+
+    @GET
+    public TemplateInstance listAction() {
+        return Templates.list().data("documents", "documents");
+    }
+}
